d3d144faa3486e7fbe480b5a4d056f0ec53fc517
benoitriandiere <86202894+benoitriandiere@users.noreply.github.com>
2021-06-20 20:49:55 +0200

    Create fonction.h

---
+++ fonction.h
@@ -0,0 +1,5 @@
+#include <stddef.h>
+void cesarch(char* msg, short cleph);
+void cesardech(char* msg, int cleph);
+void vigenerech (char * msg, char * cle);
+void vigeneredech(char * msg, char * cle);
